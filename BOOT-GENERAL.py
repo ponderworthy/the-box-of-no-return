625dc7f473e8f5f3d48e0e977116d225ef2d761d
jebofponderworthy <jeb@ponderworthy.com>
2018-02-14 22:40:17 -0600

    Continued development.

--- BOOT-GENERAL.py
+++ BOOT-GENERAL.py
@@ -362,6 +362,16 @@ if not jpctrl.spawn_and_settle(
 print('\n')
 
 print('-----------------------------------------------------------------')
+print('Start component for patch Many-Horns, on server SOFT3...')
+print('-----------------------------------------------------------------')
+
+print('\nStart Many-Horns...')
+if not jpctrl.spawn_and_settle(
+        'calfjackhost --client Many-Horns fluidsynth:Many-Horns',
+        'SOFT3'):
+    jpctrl.exit_with_beep()
+
+print('-----------------------------------------------------------------')
 print('Create JACK connections using aj-snapshot, on all servers...')
 print('-----------------------------------------------------------------')
 
